8e7faec338faa2896f8223d106eb00bbb34ba485
Denis <99001435+sinedfq@users.noreply.github.com>
2023-05-09 16:27:52 +0700

    Program update

--- Finder.py
+++ Finder.py
@@ -4,24 +4,48 @@ import string as str
 with open("ParseResult.json") as jsondata:
     data = js.load(jsondata)
 
-temp = 1
-search_name = ""
-requestName = input(" Find by Name - [1]\n Find by Symbol - [2]\n Input:")
+SYMBOL = 'SYMBOL'
+NAME = 'NAME'
 
-if requestName == "1":
-    search_name = str.capwords(input("\nEnter search Name: "))
+
+def print_coin_data(_search_name, _type):
+    print("===========")
     for i in data:
-        if i['coin']["NAME"] == search_name:
+        if i['coin'][_type] == _search_name:
             print("NAME COIN - ", i['coin']["NAME"])
             print("SYMBOL COIN - ", i['coin']['SYMBOL'])
             print("COIN PRICE - ", i['coin']['PRICE'])
             print("MARKET CAP - ", i['coin']['MARKET_CAP'])
 
-if requestName == "2":
-    search_name = input("\nEnter search Symbol: ").upper()
+
+def print_all_element():
     for i in data:
-        if i['coin']['SYMBOL'] == search_name:
-            print("NAME COIN - ", i['coin']["NAME"])
-            print("SYMBOL COIN - ", i['coin']['SYMBOL'])
-            print("COIN PRICE - ", i['coin']['PRICE'])
-            print("MARKET CAP - ", i['coin']['MARKET_CAP'])
+        print("===========")
+        print("NAME COIN - ", i['coin']["NAME"])
+        print("SYMBOL COIN - ", i['coin']['SYMBOL'])
+        print("COIN PRICE - ", i['coin']['PRICE'])
+        print("MARKET CAP - ", i['coin']['MARKET_CAP'])
+
+
+def print_developers():
+    print("https://vk.com/huharev1")
+    print("https://vk.com/lvzvrv")
+    print("https://vk.com/pmakhonin")
+
+
+requestName = input("Find by Name - [1]\nFind by Symbol - [2]\nAll elements - [3]\nDevelopers - [4]\nInput: ")
+while requestName != "":
+    if requestName == "1":
+        _type = NAME
+        search_name = str.capwords(input("Enter search Name: "))
+        print_coin_data(search_name, _type)
+    if requestName == "2":
+        _type = SYMBOL
+        search_name = input("\nEnter search Symbol: ").upper()
+        print_coin_data(search_name, _type)
+    if requestName == "3":
+        print_all_element()
+    if requestName == "4":
+        print_developers()
+    print("===========")
+    requestName = input("Find by Name - [1]\nFind by Symbol - [2]\nAll elements - [3]\nDevelopers - [4]\nInput: ")
